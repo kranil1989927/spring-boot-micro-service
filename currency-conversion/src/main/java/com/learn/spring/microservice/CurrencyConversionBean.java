5e779d209b0cc47ed3b05e8281ce470d0fbf3ad2
Anil Kumar <kranil.1989927@gmail.com>
2019-04-23 20:04:01 +0530

    Spring Boot Project - Currency Conversion Service

---
+++ currency-conversion/src/main/java/com/learn/spring/microservice/CurrencyConversionBean.java
@@ -0,0 +1,135 @@
+package com.learn.spring.microservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBean {
+	
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionRate;
+	private BigDecimal quantity;
+	private BigDecimal totalCalculatedAmount;
+	private int port;
+
+	public CurrencyConversionBean() {
+	}
+
+	public CurrencyConversionBean(Long id, String from, String to, BigDecimal conversionRate, BigDecimal quantity,
+			BigDecimal totalCalculatedAmount, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionRate = conversionRate;
+		this.quantity = quantity;
+		this.totalCalculatedAmount = totalCalculatedAmount;
+		this.port = port;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the from
+	 */
+	public String getFrom() {
+		return from;
+	}
+
+	/**
+	 * @param from
+	 *            the from to set
+	 */
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	/**
+	 * @return the to
+	 */
+	public String getTo() {
+		return to;
+	}
+
+	/**
+	 * @param to
+	 *            the to to set
+	 */
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	/**
+	 * @return the conversionRate
+	 */
+	public BigDecimal getConversionRate() {
+		return conversionRate;
+	}
+
+	/**
+	 * @param conversionRate
+	 *            the conversionRate to set
+	 */
+	public void setConversionRate(BigDecimal conversionRate) {
+		this.conversionRate = conversionRate;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity
+	 *            the quantity to set
+	 */
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the totalCalculatedAmount
+	 */
+	public BigDecimal getTotalCalculatedAmount() {
+		return totalCalculatedAmount;
+	}
+
+	/**
+	 * @param totalCalculatedAmount
+	 *            the totalCalculatedAmount to set
+	 */
+	public void setTotalCalculatedAmount(BigDecimal totalCalculatedAmount) {
+		this.totalCalculatedAmount = totalCalculatedAmount;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port
+	 *            the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+}
